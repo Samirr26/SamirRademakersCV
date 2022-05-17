f2960161a8a17a6bad7267e735d366973b73eccb
Samir <samir.rademakers3@gmail.com>
2022-05-17 21:25:35 +0200

    [Update] Working router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomepageComponent } from './components/homepage/homepage.component';
+
+const routes: Routes = [
+  {path: '', component: HomepageComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
