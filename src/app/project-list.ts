a5711247651b066209766286018d703dc27f2b11
Samir <samir.rademakers2@gmail.com>
2022-05-31 10:58:56 +0200

    [Update] Added Estelling project

--- src/app/project-list.ts
+++ src/app/project-list.ts
@@ -15,15 +15,15 @@ export const Projects: Project[] = [
   },
   {
     title: "CV Website",
-    description: "A website I developed to visualize my CV. It shows information about myself, the projects I have worked on, my skills and my contact details.",
+    description: "A website I developed to visualize my CV. It shows information about myself, the projects I am proud of, my skills and my contact details.",
     skills: ["Angular","HTML","CSS","Typescript"],
     pictures:[ "https://cdn.discordapp.com/attachments/616314074547879938/980939339318054952/unknown.png"]
   },
   {
     title: "Estelling The Card Game",
-    description: "",
+    description: "At the end of my first year in university I developed, together with a group, a card game for mobile phones. The game was meant for visitors of the Efteling, an amusement park in the Netherlands. The app was created to give the visitors a better experience in the park. With the game we developed, users can play a card game against fairytail characters from the Efteling. All they have to do is scan a code with their phone at certain place in the park. This will lead them into playing a card game on their phone against the corresponding character of the place where they scanned the code. Everytime you beat a new character for the first time, the game will give you a new card that u can use in your future games. This way you can try to get as many cards as you can. You can kinda compare it to Pokemon cards.",
     skills: ["Android"],
     pictures: ["https://cdn.discordapp.com/attachments/616314074547879938/980954232633057370/unknown.png",
-    "https://cdn.discordapp.com/attachments/616314074547879938/980954999548936272/unknown.png"]
+    "https://cdn.discordapp.com/attachments/616314074547879938/981115645666148352/unknown.png","https://cdn.discordapp.com/attachments/616314074547879938/981115833214468126/unknown.png"]
   }
 ]
