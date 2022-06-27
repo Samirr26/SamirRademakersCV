65ebdeb13e06d67f7c69d2cadfc0b662af5f57eb
Samir <samir.rademakers3@gmail.com>
2022-06-27 17:33:01 +0200

    [Update] Changed navigation bar

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  copyString: string = 'Click to copy';
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  copyText(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+
+    this.copyString = 'Copied!'
+  }
 }
